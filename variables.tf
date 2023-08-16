c39f0d5ff21eb64d086edd7a3c9de0281c0e6f94
DedeMarantes <andre.marantes@gmail.com>
2023-08-16 20:21:42 -0300

    variaveis

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "cidr_subnet" {
   type    = list(string)
-  default = ["10.0.0.0/20", "10.0.16.0/20"]
+  //default = ["10.0.0.0/20", "10.0.16.0/20"]
 }
 
 variable "zones" {
   type    = list(string)
-  default = ["us-east-1a", "us-east-1b"]
+  //default = ["us-east-1a", "us-east-1b"]
 }
 
 variable "instance-type" {
-  default = "t2.micro"
+  //default = "t2.micro"
 }
 
